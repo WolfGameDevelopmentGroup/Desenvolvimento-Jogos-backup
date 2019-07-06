26938fd27f3dc9640c8f8d2425315ded074a1932
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-07-06 15:45:20 -0300

    Estudo sobre rotação e animações com Graphics2D do java

---
+++ Animacoes_rotacionar_objetos/Game/FrameGame.java
@@ -0,0 +1,59 @@
+/*
+	 FrameGame.java (java)
+	 
+	 Objetivo: Gerar o Jframe (Tela de diálogo) de um programa no java.
+	 
+	 Versão 1.0
+	 
+	 Site: http://www.dirackslounge.online
+	 
+	 Programador: Rodolfo A. C. Neves (Dirack) 23/06/2019
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 Licença: Software de uso livre e código aberto.
+*/
+
+package Game;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class FrameGame extends Canvas{
+
+	public static JFrame frame;
+	public static Canvas canvas;
+	private int HEIGHT;
+	private int WIDTH;
+	private int SCALE;
+	private String FrameTitle;
+
+	public FrameGame(int HEIGHT, int WIDTH, int SCALE, String FrameTitle){
+
+		this.HEIGHT = HEIGHT;
+		this.WIDTH = WIDTH;
+		this.SCALE = SCALE;
+		this.FrameTitle = FrameTitle;
+
+	}
+
+	public void startGameFrame(){
+
+		this.setPreferredSize(new Dimension(this.WIDTH*this.SCALE,this.HEIGHT*this.SCALE));
+		this.frame = new JFrame(this.FrameTitle);
+
+		this.canvas = new Canvas();
+		this.canvas.setPreferredSize(new Dimension(this.WIDTH*this.SCALE,this.HEIGHT*this.SCALE));
+
+		this.frame.add(canvas);
+		this.frame.setResizable(false);
+		this.frame.pack();
+		this.frame.setLocationRelativeTo(null);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setVisible(true);
+
+	}
+
+}
